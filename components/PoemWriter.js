74645e54ac5f7c58e5eb809936f14b7894253c6a
tao liu <werewaswas@live.com>
2017-06-04 04:23:19 +0000

    Done.

--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -1,17 +1,45 @@
 import React from 'react';
 
+function validatePoem(poem) {
+   let poemArr = poem.split('\n').filter(function(x) {return x});
+  return (
+          validateLine(poemArr[0], 5) &&
+          validateLine(poemArr[1], 3) &&
+          validateLine(poemArr[2], 5) &&
+          poemArr.length === 3
+        );
+}
+
+function validateLine(line, wordCount) {
+    if (line) {
+      return line.split(' ').filter(function(x) {return x}).length < wordCount
+    } else {
+      return false
+    }
+}
+
 export default class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {value: ' '};
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    if (event.target.value) {
+          this.setState({
+          value: event.target.value,
+          validPoem: validatePoem( event.target.value)
+          });
+      }
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea rows="3" cols="60" onChange={this.handleChange} value={this.state.value}/>
+        {this.state.validPoem ? : <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>}
       </div>
     );
   }
--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -3,15 +3,21 @@ import React from 'react';
 export default class TwitterMessage extends React.Component {
   constructor() {
     super();
-
-    this.state = {};
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this)
   }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <strong>{this.props.maxChars - this.state.value.length} characters left.</strong>
+        <input type="text" onChange={this.handleChange} value={this.state.value}/>
       </div>
     );
   }
